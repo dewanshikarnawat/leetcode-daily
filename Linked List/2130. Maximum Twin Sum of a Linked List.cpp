e92060f8f2ecc311d2d26d126f0e741a7546d741
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-06-14 09:58:55 +0530

    Add solution for Maximum Twin Sum of a Linked List
    Implement solution for finding maximum twin sum of a linked list.

---
+++ Linked List/2130. Maximum Twin Sum of a Linked List.cpp
@@ -0,0 +1,39 @@
+//Problem: 2130. Maximum Twin Sum of a Linked List
+//Topic: Linked List
+//Pattern: Medium
+
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Find middle
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Calculate maximum twin sum
+        int ans = 0;
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            ans = max(ans, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return ans;
+    }
+};
