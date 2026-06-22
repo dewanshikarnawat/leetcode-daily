5a9cfb9563abe8b8872b7b0a9ac2ede3f0c5f72c
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-06-22 23:13:51 +0530

    Add solution for maximum number of balloons problem
    Implement solution to find the maximum number of 'balloon' words that can be formed from the given text by counting character frequencies.

---
+++ String+Greedy+Hashing/1189. Maximum Number of Balloons Solution.cpp
@@ -0,0 +1,19 @@
+//Problem: 1189. Maximum Number of Balloons Solution
+//Topic: String, Hashing (Frequency Counting), Greedy
+//Pattern: Easy
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> freq(26, 0);
+
+        for (char c : text)
+            freq[c - 'a']++;
+
+        return min({freq['b' - 'a'],
+                    freq['a' - 'a'],
+                    freq['l' - 'a'] / 2,
+                    freq['o' - 'a'] / 2,
+                    freq['n' - 'a']});
+    }
+};
