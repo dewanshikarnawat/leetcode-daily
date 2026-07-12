2286976e97d31e2c4614504fca845c58686d40cb
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-12 19:30:02 +0530

    Create 1331. Rank Transform of an Array.cpp

---
+++ Arrays/1331. Rank Transform of an Array.cpp
@@ -0,0 +1,27 @@
+//Problem: 1331. Rank Transform of an Array
+//Topic: Array + Sorting
+//Pattern: Easy
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp = arr;
+        sort(temp.begin(), temp.end());
+
+        unordered_map<int, int> rank;
+        int r = 1;
+
+        for (int x : temp) {
+            if (rank.find(x) == rank.end()) {
+                rank[x] = r++;
+            }
+        }
+
+        vector<int> ans;
+        for (int x : arr) {
+            ans.push_back(rank[x]);
+        }
+
+        return ans;
+    }
+};
