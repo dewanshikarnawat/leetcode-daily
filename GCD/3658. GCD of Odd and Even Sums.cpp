d63a9a91bfac71fb05d68722848dc1924d21a78a
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-15 12:23:22 +0530

    Create 3658. GCD of Odd and Even Sums.cpp

---
+++ GCD/3658. GCD of Odd and Even Sums.cpp
@@ -0,0 +1,10 @@
+//Problem: 3658. GCD of Odd and Even Sums
+//Topic: GCD (Greatest Common Divisor)
+//Pattern: Easy
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
