a302079cb81ecc57bf5027e403876285e8ac9ab8
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-25 20:14:38 +0530

    Create 3536. Maximum Product of Two Digits.cpp

---
+++ Math/3536. Maximum Product of Two Digits.cpp
@@ -0,0 +1,26 @@
+//Problem: 3536. Maximum Product of Two Digits
+//Topic: Math + Brute Force
+//Pattern: Easy
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector<int> digits;
+        
+        while (n > 0) {
+            digits.push_back(n % 10);
+            n /= 10;
+        }
+        
+        int ans = 0;
+        int m = digits.size();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
+                ans = max(ans, digits[i] * digits[j]);
+            }
+        }
+        
+        return ans;
+    }
+};
