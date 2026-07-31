10eb947eae658334c129bacf26f2daffa69a7486
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-07-31 23:27:01 +0530

    Create 3016. Minimum Number of Pushes to Type Word II.cpp

---
+++ Greedy/3016. Minimum Number of Pushes to Type Word II.cpp
@@ -0,0 +1,24 @@
+//Problem: 3016. Minimum Number of Pushes to Type Word II
+//Topic: Greedy
+//Pattern: Medium
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for (char c : word)
+            freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) break;
+            ans += freq[i] * (i / 8 + 1);
+        }
+
+        return ans;
+    }
+};
