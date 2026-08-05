040ef5210573ff15f5f7ac5e16c8fe4a89d320a6
DewanshiKarnawat <dewanshikarnawat@gmail.com>
2026-08-05 22:42:11 +0530

    Create 3310. Remove Methods From Project.cpp

---
+++ Graph/3310. Remove Methods From Project.cpp
@@ -0,0 +1,54 @@
+//Problem: 3310. Remove Methods From Project
+//Topic: DFS/BFS (Graph Traversal)
+//Pattern: Medium
+
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+
+        for (auto &e : invocations) {
+            graph[e[0]].push_back(e[1]);
+        }
+
+        // Find all suspicious methods (reachable from k)
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(k);
+        vis[k] = 1;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v : graph[u]) {
+                if (!vis[v]) {
+                    vis[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+
+        // Check if any outside method invokes a suspicious method
+        for (auto &e : invocations) {
+            int u = e[0];
+            int v = e[1];
+
+            if (!vis[u] && vis[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        // Remove suspicious methods
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
